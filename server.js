757a521a26c0b5a1e2af50e30c247dc2a8a31728
charisse-atillo <65036763+charisse-atillo@users.noreply.github.com>
2020-05-08 14:51:23 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+// create a server variable
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.end("Charisse Atillo - This is my first response");
+});
+
+const port = process.env.PORT || "3000";
+
+console.log("Running on port " + port);
+// listen at a port
+server.listen(port);
